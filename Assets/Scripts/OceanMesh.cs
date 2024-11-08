c20ca2a3c9aef4c666fbe4f52ee9afa1c56471bd
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-11-08 11:26:24 -0800

    start of ocean shader

--- Assets/Scripts/OceanMesh.cs
+++ Assets/Scripts/OceanMesh.cs
@@ -2,33 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Experimental.GlobalIllumination;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 public class OceanMesh : MonoBehaviour
 {
     public MeshFilter meshFilter;
     public OceanMapGenerator oceanMapGenerator;
     public Shader oceanShader;
+    public Light sun;
     public int meshLength = 10;
-    private int meshResolution;
     private Mesh mesh;
-    private Vector3[] vertices;
-    private Vector3[] normals;
-    private int[] triangles;
-    private Vector2[] uvs;
     private Material oceanMaterial;
-
-    Vector3[] generateMeshVerticesPlane(int m, int n, float width, float height)
-    {
-        Vector3[] v = new Vector3[m * n];
-        for (int i = 0; i < m * n; i++)
-        {
-            float x = (i % m) * (width / (m - 1.0f)) - width / 2.0f;
-            float z = (i / m) * (height / (n - 1.0f)) - height / 2.0f;
-            v[i] = new Vector3(x, 0, z);
-        }
-        return v;
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -37,11 +23,31 @@ public class OceanMesh : MonoBehaviour
         mesh.name = "Ocean Surface";
         GetComponent<MeshFilter>().mesh = mesh;
 
+        oceanMaterial = new Material(oceanShader);
+        oceanMaterial.SetTexture("_DisplacementTex", oceanMapGenerator.displacement);
+        oceanMaterial.SetTexture("_SlopeTex", oceanMapGenerator.slope);
+        oceanMaterial.SetVector("_LightDir", -sun.gameObject.transform.forward);
+        GetComponent<MeshRenderer>().material = oceanMaterial;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void OnApplicationQuit()
+    {
+        if (oceanMaterial)
+        {
+            Destroy(oceanMaterial);
+            oceanMaterial = null;
+        }
+        if (mesh)
+        {
+            Destroy(mesh);
+            mesh = null;
+        }
     }
 }
--- Assets/Shaders/OceanShader.shader
+++ Assets/Shaders/OceanShader.shader
@@ -3,6 +3,7 @@ Shader "Parker/OceanShader"
     Properties
     {
         _DisplacementTex ("Texture", 2D) = "white" {}
+        _SlopeTex ("Texture", 2D) = "white" {}
     }
     SubShader
     {
@@ -16,6 +17,7 @@ Shader "Parker/OceanShader"
             #pragma fragment frag
 
             #include "UnityCG.cginc"
+            #include "UnityStandardBRDF.cginc"
 
             struct appdata
             {
@@ -30,12 +32,14 @@ Shader "Parker/OceanShader"
             };
 
             sampler2D _DisplacementTex;
-            float4 _DisplacementTex_ST;
+            sampler2D _SlopeTex;
+            float3 _LightDir;
 
             v2f vert (appdata v)
             {
                 v2f o;
-                float3 displacement = tex2Dlod(_DisplacementTex, float4(v.uv,0,0)).rgb;
+                float3 displacement = tex2Dlod(_DisplacementTex, float4(v.uv,0,0)).rgb / 1024.0;
+                v.vertex.xyz += mul(unity_WorldToObject, displacement.xyz);
                 o.vertex = UnityObjectToClipPos(v.vertex);
                 o.uv = v.uv;
                 return o;
@@ -43,8 +47,15 @@ Shader "Parker/OceanShader"
 
             fixed4 frag (v2f i) : SV_Target
             {
-                float4 col = float4(1,1,1,1);
-                return col;
+                float2 slope = tex2D(_SlopeTex, i.uv);
+                float3 normal = normalize(float3(-slope.x, 1.0, -slope.y));
+                normal = normalize(UnityObjectToWorldNormal(normalize(normal)));
+
+                float ndotl = DotClamped(_LightDir, normal);
+
+                float4 col = float4(0,0,0.7, 1.0);
+                
+                return col * ndotl + col * 0.3;
             }
             ENDCG
         }
